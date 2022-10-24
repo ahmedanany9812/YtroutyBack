20cc4048bbb6db6bd526af076a6cb02e14bf0bb0
Ahmed anany <ahmedenany9812@gmail.com>
2022-10-24 21:59:02 +0200

    Refactor: Update App.js configuration

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://ytrouty.onrender.com",
+    origin: "https://ytrouty-app.onrender.com",
     credentials: true,
   })
 );
